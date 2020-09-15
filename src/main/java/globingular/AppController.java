bf7554f7dc769f12660b0d4218ae58ddc3aea399
Helga N <helgamn@stud.ntnu.no>
2020-09-15 20:47:59 +0200

    Add trancoder with batik

--- src/main/java/globingular/AppController.java
+++ src/main/java/globingular/AppController.java
@@ -2,11 +2,42 @@ package globingular;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.embed.swing.SwingFXUtils;
+import org.apache.batik.transcoder.TranscoderException;
+import org.apache.batik.transcoder.TranscoderInput;
+
+
+import java.io.IOException;
+import java.io.InputStream;
 
 public class AppController {
     
     @FXML
     Button clickMeButton;
+
+    @FXML
+    ImageView imgView;
+
+    public void initialize() {
+        BufferedImageTranscoder transcoder = new BufferedImageTranscoder();
+        try (InputStream file = getClass().getResourceAsStream("https://raw.githubusercontent.com/raphaellepuschitz/SVG-World-Map/master/src/world-states.svg")) {
+            TranscoderInput transcoderIn = new TranscoderInput(file);
+            try {
+                transcoder.transcode(transcoderIn, null);
+                Image img = SwingFXUtils.toFXImage(transcoder.getBufferedImage(), null);
+                imgView.setImage(img);
+
+            } catch (TranscoderException e) {
+                e.printStackTrace();
+            }
+        }
+        catch (IOException io){
+            io.printStackTrace();
+        }
+
+    }
 
     @FXML
     void handleClickMeButtonAction() {
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,8 +2,13 @@ module globingular {
 	requires javafx.fxml;
 	requires transitive javafx.graphics;
 	requires javafx.controls;
-	
+	requires java.desktop;
+	requires batik.transcoder;
+	requires batik.bridge;
+	requires javafx.swing;
+
 	exports globingular;
 
-	opens globingular to javafx.fxml;
+	opens globingular to javafx.fxml, batik.bridge, batik.transcoder;
+
 }
