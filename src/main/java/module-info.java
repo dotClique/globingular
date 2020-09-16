7fc9ef463f4128bf0575f3c16adf8a0afe37c97f
Dentsor <dentsor@dentsor.net>
2020-09-16 12:04:40 +0200

    Fix Batik and dependencies

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -6,7 +6,27 @@ module globingular {
 	requires batik.transcoder;
 	requires batik.bridge;
 	requires batik.dom;
+	requires batik.css;
+	requires batik.gvt;
+	requires batik.util;
+	requires batik.anim;
+	requires batik.i18n;
+	requires batik.svg.dom;
+	requires batik.xml;
+	requires batik.constants;
+	requires batik.awt.util;
+	requires batik.ext;
+	requires batik.parser;
+	requires batik.script;
+	requires batik.svggen;
+	requires xml.apis.ext;
+	requires xmlgraphics.commons;
+	requires jdk.xml.dom;
 	requires javafx.swing;
+
+	uses org.apache.batik.bridge.RhinoInterpreterFactory;
+
+	// provides org.apache.batik.script.InterpreterFactory with org.apache.batik.bridge.RhinoInterpreterFactory;
 
 	exports globingular;
 
