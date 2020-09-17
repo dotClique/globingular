c3959b496bce7015077043e8a5bb942322650417
Dentsor <dentsor@dentsor.net>
2020-09-17 15:18:49 +0200

    Move CountryCollector to globingular.core

--- globingular/src/main/java/globingular/core/CountryCollector.java
+++ globingular/src/main/java/globingular/core/CountryCollector.java
@@ -1,4 +1,4 @@
-package globingular;
+package globingular.core;
 
 import java.util.HashSet;
 import java.util.List;
--- globingular/src/test/java/globingular/core/CountryCollectorTest.java
+++ globingular/src/test/java/globingular/core/CountryCollectorTest.java
@@ -1,4 +1,4 @@
-package globingular;
+package globingular.core;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
