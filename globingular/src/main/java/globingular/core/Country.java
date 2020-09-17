6145c03b87f7bebddb339b5b4573be5aa5681dfd
Andrine Bergan <andrinbe@stud.ntnu.no>
2020-09-17 20:16:20 +0200

    Add Country class

---
+++ globingular/src/main/java/globingular/core/Country.java
@@ -0,0 +1,42 @@
+package globingular.core;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+public class Country {
+    private String countryCode;
+    private String name;
+    private String longname;
+    private String sovereignty;
+    private String region;
+    private long population;
+    private Province[] provinces;
+
+    public String getCountryCode() {
+        return countryCode;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getLongname() {
+        return longname;
+    }
+
+    public String getSovereignty() {
+        return sovereignty;
+    }
+
+    public String getRegion() {
+        return region;
+    }
+
+    public long getPopulation() {
+        return population;
+    }
+
+    @Override
+    public String toString() {
+        return CountriesSerializer.serializeCountry(this);
+    }
+}
