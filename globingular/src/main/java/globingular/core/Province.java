9484f94954de78525c0aa88fb5bfcb0733447dba
Andrine Bergan <andrinbe@stud.ntnu.no>
2020-09-17 20:17:12 +0200

    Add Province class

---
+++ globingular/src/main/java/globingular/core/Province.java
@@ -0,0 +1,29 @@
+package globingular.core;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Province {
+
+    private String provinceCode;
+    private String capital;
+    private String name;
+    private long population;
+
+    public String getProvinceCode() {
+        return provinceCode;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getCapital() {
+        return capital;
+    }
+
+    public long getPopulation() {
+        return population;
+    }
+}
