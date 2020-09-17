7942fef49457baa653642fe4458a48f22b572b8b
Andrine Bergan <andrinbe@stud.ntnu.no>
2020-09-17 20:15:16 +0200

    Add countrySerializer class

---
+++ globingular/src/main/java/globingular/json/countriesSerializer.java
@@ -0,0 +1,15 @@
+package globingular.json;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+public class CountriesSerializer {
+    public static String serializeCountry(Country c) {
+        ObjectMapper om = new ObjectMapper();
+        try {
+            return om.writeValueAsString(c);
+        } catch (JsonProcessingException e) {
+            return e.getMessage();
+        }
+    }
+}
