2978c1234de2eb44373a62bf3cb825c0b8c2a645
Dentsor <dentsor@dentsor.net>
2020-09-17 22:49:20 +0200

    Implement PersistenceHandler

---
+++ globingular/src/main/java/globingular/json/PersistenceHandler.java
@@ -0,0 +1,64 @@
+package globingular.json;
+
+import java.io.BufferedInputStream;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.Writer;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import globingular.core.CountryCollector;
+
+public class PersistenceHandler {
+
+    final private Path FILE_COLLECTOR = Paths.get(System.getProperty("user.home"), "temp", "globingular", "countryCollector.json");
+    final private Path DATA_FOLDER = FILE_COLLECTOR.getParent();
+    final private String SAMPLE_COLLECTOR = "/json/sampleCollector.json";
+
+    private ObjectMapper objectMapper;
+
+    private ObjectMapper getObjectMapper() {
+        if (objectMapper == null) {
+            objectMapper = new ObjectMapper();
+            objectMapper.registerModule(new CountryCollectorModule());
+        }
+        return objectMapper;
+    }
+
+    public CountryCollector loadState() {
+        CountryCollector countryCollector = null;
+        try (InputStream in = new BufferedInputStream(new FileInputStream(FILE_COLLECTOR.toFile()))) {
+            countryCollector = this.getObjectMapper().readValue(in, CountryCollector.class);
+        } catch (FileNotFoundException e) {
+            try (InputStream in = getClass().getResourceAsStream(SAMPLE_COLLECTOR)) {
+                countryCollector = this.getObjectMapper().readValue(in, CountryCollector.class);
+            } catch (IOException err) {
+                err.printStackTrace();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return countryCollector;
+    }
+
+    public CountryCollector saveState(CountryCollector countryCollector) {
+        try {
+            Files.createDirectories(DATA_FOLDER);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        try (Writer out = Files.newBufferedWriter(FILE_COLLECTOR)) {
+            this.getObjectMapper().writerWithDefaultPrettyPrinter().writeValue(out, countryCollector);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return countryCollector;
+    }
+}
