eeafdec997f192755363b748494be21c6ce102a8
Dentsor <dentsor@dentsor.net>
2020-09-28 20:55:26 +0200

    Fix checkstyle in Core-package

--- globingular/src/main/java/globingular/core/CountryCollector.java
+++ globingular/src/main/java/globingular/core/CountryCollector.java
@@ -6,65 +6,73 @@ import javafx.collections.FXCollections;
 
 public class CountryCollector {
 
-    final private SetProperty<String> visits;
+    /**
+     * Set of visited countries, using countryCodes according to the ISO 3166-1
+     * alpha-2 standard. Using SetProperty for observability and binding.
+     */
+    private final SetProperty<String> visits;
 
     /**
-     * Defines a new CountryCollector-object without any visits
+     * Defines a new CountryCollector-object without any visits.
      */
     public CountryCollector() {
         this.visits = new SimpleSetProperty<>(FXCollections.observableSet());
     }
 
     /**
-     * Defines a new CountryCollector-object with the given countries set as visited
+     * Defines a new CountryCollector-object with the given countries set as
+     * visited.
      * 
      * @param countries Array of countries that has been visited
      */
-    public CountryCollector(String... countries) {
+    public CountryCollector(final String... countries) {
         this();
         this.visits.set(FXCollections.observableSet(countries));
     }
 
     /**
-     * Set visitation status of the given country
+     * Set visitation status of the given country.
      * 
-     * @param countryCode The two-letter country code to log
+     * @param countryCode The two-letter country code to set as visited. Using ISO
+     *                    3166-1 alpha-2 codes
      */
-    public void setVisited(String countryCode) {
+    public void setVisited(final String countryCode) {
         this.visits.add(countryCode);
     }
 
     /**
-     * Remove visitation status of the given country
+     * Remove visitation status of the given country.
      * 
-     * @param countryCode The two-letter country code to log
+     * @param countryCode The two-letter country code to remove from visited
+     *                    countries. Using ISO 3166-1 alpha-2 codes
      */
-    public void removeVisited(String countryCode) {
+    public void removeVisited(final String countryCode) {
         this.visits.remove(countryCode);
     }
 
     /**
-     * Check if the given country has been visited
+     * Check if the given country has been visited.
      * 
-     * @param countryCode The two-letter country code to check
+     * @param countryCode The two-letter country code to check. Using ISO 3166-1
+     *                    alpha-2 codes
      * @return Returns true if the country has been visited. Returns false for all
      *         non-logged, including invalid names.
      */
-    public boolean hasVisited(String countryCode) {
+    public boolean hasVisited(final String countryCode) {
         return this.visits.contains(countryCode);
     }
 
     /**
-     * Get a list with all the countryCodes of the countries that has been visited
+     * Get a list with all the countryCodes of the countries that has been visited.
      * 
-     * @return List of countryCodes visited
+     * @return List of countryCodes visited. Using ISO 3166-1 alpha-2 codes
      */
     public String[] getVisitedCountries() {
         return visits.toArray(new String[this.numberVisited()]);
     }
 
     /**
-     * Get the amount of countries visited
+     * Get the amount of countries visited.
      * 
      * @return Returns number of countries visited
      */
@@ -72,6 +80,10 @@ public class CountryCollector {
         return this.visits.size();
     }
 
+    /**
+     * Return a plain text representation of the object. Only used as a
+     * human-readable representation. Not a stable interface.
+     */
     @Override
     public String toString() {
         return this.visits.getValue().toString();
--- globingular/src/main/java/globingular/core/Province.java
+++ globingular/src/main/java/globingular/core/Province.java
@@ -2,23 +2,59 @@ package globingular.core;
 
 public class Province {
 
+    /**
+     * The short-code of the Province, following the ISO 3166-2 standard for states
+     * and provinces.
+     */
     private String provinceCode;
+
+    /**
+     * The capital of the Province.
+     */
     private String capital;
+
+    /**
+     * The full name of the Province.
+     */
     private String name;
+
+    /**
+     * The population of the Province.
+     */
     private long population;
 
+    /**
+     * Get the Province's short-code.
+     * 
+     * @return The province code, following the ISO 3166-2 standard
+     */
     public String getProvinceCode() {
         return provinceCode;
     }
 
+    /**
+     * Get the Province's full name.
+     * 
+     * @return The full name of the Province
+     */
     public String getName() {
         return name;
     }
 
+    /**
+     * Get the Province's capital.
+     * 
+     * @return The capital of the Province
+     */
     public String getCapital() {
         return capital;
     }
 
+    /**
+     * Get the Province's population.
+     * 
+     * @return The population of the Province
+     */
     public long getPopulation() {
         return population;
     }
