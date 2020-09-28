7b4d87197d50b24822730d2a5398c8996bb461eb
Lars Mitsem Selbekk <lars@selbekk.com>
2020-09-28 18:56:32 +0200

    Add package-info.java to all packages

---
+++ globingular/src/main/java/globingular/core/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * Package containing the core functionality of the Globingular app including business
+ * logic, data structures and data models.
+ */
+package globingular.core;
---
+++ globingular/src/main/java/globingular/ui/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Package containing the UI/display-code for the Globingular app, using JavaFX.
+ */
+package globingular.ui;
