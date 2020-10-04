f99fe2c73cfc6157f51aaeb35422e419b0a50dfd
Andrine Bergan <andrinbe@stud.ntnu.no>
2020-10-04 13:32:31 +0000

    Separate src into three modules; core, fxui, persistance

---
+++ globingular/fxui/src/main/java/module-info.java
@@ -0,0 +1,19 @@
+module globingular {
+	requires javafx.fxml;
+	requires transitive javafx.graphics;
+	requires javafx.controls;
+	requires javafx.base;
+	
+	requires transitive com.fasterxml.jackson.core;
+    requires transitive com.fasterxml.jackson.databind;
+	requires java.desktop;
+	requires javafx.swing;
+
+	/* Batik-imports */
+	requires batik.transcoder;
+	requires batik.anim;
+	requires batik.util;
+	requires batik.dom;
+
+	opens globingular.ui to javafx.fxml, javafx.graphics;
+}
