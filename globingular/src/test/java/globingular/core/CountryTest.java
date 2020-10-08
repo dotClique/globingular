7a54cdec012797f57641b4a8eeb681e19a6c78b0
Helga N <helgamn@stud.ntnu.no>
2020-10-08 14:00:46 +0200

    Add countrytest

---
+++ globingular/src/test/java/globingular/core/CountryTest.java
@@ -0,0 +1,82 @@
+package globingular.core;
+
+import javafx.stage.Stage;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.testfx.framework.junit5.ApplicationTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+public class CountryTest extends ApplicationTest {
+
+    private Country country;
+
+    @Override
+    public void start(final Stage stage) {
+       country = mock(Country.class);
+       when(country.getCountryCode()).thenReturn("NO");
+       when(country.getName()).thenReturn("Norway");
+       when(country.getLongname()).thenReturn("The Kingdom of Norway");
+       when(country.getSovereignty()).thenReturn("UN");
+       when(country.getRegion()).thenReturn("EU");
+       when(country.getPopulation()).thenReturn((long) 5367580);
+    }
+
+    @Test
+    public void testGetCountryCode() {
+
+        String result = country.getCountryCode();
+
+        Assertions.assertEquals("NO", result);
+        Assertions.assertNotNull(country.getCountryCode());
+
+    }
+
+    @Test
+    public void testName() {
+
+        String result = country.getName();
+
+        assertThat(result).isEqualTo("Norway");
+        Assertions.assertNotNull(country.getName());
+
+    }
+
+    @Test
+    public void testLongName() {
+
+        String result = country.getLongname();
+
+        Assertions.assertEquals("The Kingdom of Norway", result);
+        Assertions.assertNotNull(country.getLongname());
+    }
+
+    @Test
+    public void testSovereignty() {
+
+        String result = country.getSovereignty();
+
+        Assertions.assertEquals("UN", result);
+        Assertions.assertNotNull(country.getSovereignty());
+    }
+
+    @Test
+    public void testGetRegion() {
+
+        String result = country.getRegion();
+
+        Assertions.assertEquals("EU", result);
+        Assertions.assertNotNull(country.getRegion());
+    }
+
+    @Test
+    public void testGetPopulation() {
+
+        Long result = country.getPopulation();
+
+        Assertions.assertEquals( 5367580, result);
+
+    }
+}
