da92b325bae41f761b30f8a88363a7306a0c9b9b
Andrine Bergan <andrinbe@stud.ntnu.no>
2020-11-02 08:41:02 +0000

    Add CountryStatisticsTest.java in core tests

---
+++ globingular/core/src/test/java/globingular/core/CountryStatisticsTest.java
@@ -0,0 +1,47 @@
+package globingular.core;
+
+import globingular.core.World;
+import globingular.core.CountryCollector;
+import globingular.core.CountryStatistics;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * Class that tests the implementation of country statistics.
+ */
+public class CountryStatisticsTest {
+    static Country country0, country1;
+    static World world0;
+    static CountryCollector collector;
+
+     @BeforeAll
+    public static void start() {
+        country0 = new Country("AD", "Andorra", "The Principality of Andorra", "UN", "EU", 77543, new Province[0]);
+        country1 = new Country("GF", "French Guiana", "Guyane", "France", "SA", 290601, new Province[0]);
+        world0 = new World(country0, country1);
+        collector = new CountryCollector(world0);
+    }
+    
+    @Test
+    public void testGetNrOfVisitedCountries() {
+        CountryStatistics cs = new CountryStatistics();
+        collector.removeVisited(country0);
+        collector.removeVisited(country1);
+        assertEquals("0", cs.getNrOfVisitedCountries(collector));
+        collector.setVisited(country0);
+        assertEquals("1", cs.getNrOfVisitedCountries(collector));
+    }
+
+    @Test
+    public void testGetMostPopulatedVisitedCountry() {
+        collector.removeVisited(country0);
+        collector.removeVisited(country1);
+        CountryStatistics cs = new CountryStatistics();
+        collector.setVisited(country0);
+        assertEquals("Andorra (77543)", cs.getMostPopulatedVisitedCountry(collector));
+        collector.setVisited(country1);
+        assertEquals("French Guiana (290601)", cs.getMostPopulatedVisitedCountry(collector));
+    }
+}
