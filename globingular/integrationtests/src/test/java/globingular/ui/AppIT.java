b46a5e4aaeca507f2c289747191d76a03a518539
andrinbe <andrinbe@stud.ntnu.no>
2020-11-20 16:12:15 +0100

    Add some more integration tests in AppIT.java

--- globingular/integrationtests/src/test/java/globingular/ui/AppIT.java
+++ globingular/integrationtests/src/test/java/globingular/ui/AppIT.java
@@ -16,14 +16,25 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.testfx.framework.junit5.ApplicationTest;
 
+import globingular.core.Country;
+import globingular.core.CountryCollector;
+import javafx.scene.control.ScrollPane;
+import org.junit.jupiter.api.Assertions;
+import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
 public class AppIT extends ApplicationTest {
 
+    private Parent parent;
     private AppController controller;
 
     @Override
     public void start(final Stage primaryStage) throws IOException {
         final FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/AppIT.fxml"));
-        final Parent parent = loader.load();
+        parent = loader.load();
         this.controller = loader.getController();
         primaryStage.setScene(new Scene(parent, 1300, 600));
         primaryStage.show();
@@ -44,5 +55,66 @@ public class AppIT extends ApplicationTest {
     @Test
     public void testController_initial() {
         assertNotNull(this.controller);
+    }
+
+    @Test
+    public void testController() {
+        final ListView<Country> countriesList = (ListView<Country>) parent.lookup(
+                "#countriesList");
+        final CountryCollector cc = controller.getCountryCollector();
+        assertNotNull(cc);
+        assertNotNull(cc.getWorld());
+        final TextField countryInput = (TextField) parent.lookup("#countryInput");
+        final Button countryAdd = (Button) parent.lookup("#countryAdd");
+        final Button countryDel = (Button) parent.lookup("#countryDel");
+        final ScrollPane scrollPane = (ScrollPane) parent.lookup("#mapPageScroll");
+        Country au = cc.getWorld().getCountryFromCode("AU");
+
+        if (countriesList.getItems().contains(au)) {
+            countryInput.setText(au.getCountryCode());
+            scrollPane.setVvalue(scrollPane.getVmax());
+            clickOn(countryDel);
+            Assertions.assertFalse(countriesList.getItems().contains(au));
+        }
+        Assertions.assertFalse(countriesList.getItems().contains(au));
+        countryInput.setText(au.getCountryCode());
+        scrollPane.setVvalue(scrollPane.getVmax());
+        clickOn(countryAdd);
+        Assertions.assertEquals("", countryInput.getText());
+        Assertions.assertTrue(countriesList.getItems().contains(au));
+        countryInput.setText(au.getCountryCode());
+        scrollPane.setVvalue(scrollPane.getVmax());
+        clickOn(countryDel);
+        Assertions.assertFalse(countriesList.getItems().contains(au));
+    }
+
+    @Test
+    public void testServerIntegration() {
+        final TextField userInput = (TextField) parent.lookup("#userInput");
+        final Button changeUser = (Button) parent.lookup("#changeUser");
+        clickOn(changeUser);
+        userInput.setText("lovelace");
+        clickOn(changeUser);
+        Assertions.assertEquals("lovelace", userInput.getText());
+    }
+
+    @Test
+    public void checkCountriesUnclickedWhenChangeUser() {
+        final ListView<Country> countriesList = (ListView<Country>) parent.lookup(
+                "#countriesList");
+        final CountryCollector cc = controller.getCountryCollector();
+        Country jp = cc.getWorld().getCountryFromCode("JP");
+        final TextField countryInput = (TextField) parent.lookup("#countryInput");
+        final TextField userInput = (TextField) parent.lookup("#userInput");
+        countryInput.setText("Japan");
+        final Button countryAdd = (Button) parent.lookup("#countryAdd");
+        final Button changeUser = (Button) parent.lookup("#changeUser");
+        clickOn(changeUser);
+        userInput.setText("tesla");
+        clickOn(changeUser);
+        final ListView<Country> countriesList1 = (ListView<Country>) parent.lookup(
+                "#countriesList");
+        Assertions.assertFalse(countriesList.getItems().contains(jp));
+
     }
 }
